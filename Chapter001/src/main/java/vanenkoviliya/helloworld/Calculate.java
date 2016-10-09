fe58684486a9081c7f5757d7833f006137235fa5
iliya-84 <iliya-84@mail.ru>
2016-10-09 10:22:04 +0300

    new

--- Chapter001/src/main/java/vanenkoviliya/helloworld/Calculate.java
+++ Chapter001/src/main/java/vanenkoviliya/helloworld/Calculate.java
@@ -1,12 +1,7 @@
 package vanenkoviliya.helloworld;
 
 /**
- * @author vanenkov_ia
- * @version 1
- * @since 01.10.2016
- **/
+ * Created by ИЛЬЯ on 09.10.2016.
+ */
 public class Calculate {
-        public static void main(String[] args) {
-        System.out.println("Hello world");
-        }
 }
