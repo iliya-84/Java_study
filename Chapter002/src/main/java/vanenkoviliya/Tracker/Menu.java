39c85164e5c7a1dec0f211410c643c81519f83ed
iliya-84 <iliya-84@mail.ru>
2016-12-17 16:56:44 +0300

    17.12.16.

---
+++ Chapter002/src/main/java/vanenkoviliya/Tracker/Menu.java
@@ -0,0 +1,9 @@
+package vanenkoviliya.Tracker;
+
+/**
+ * @author ИЛЬЯ
+ * @version 1
+ * @since 16.12.2016
+ */
+public class Menu {
+}
---
+++ Chapter002/src/main/java/vanenkoviliya/Tracker/StubInput.java
@@ -0,0 +1,9 @@
+package vanenkoviliya.Tracker;
+
+/**
+ * @author ИЛЬЯ
+ * @version 1
+ * @since 11.12.2016
+ */
+public class StubInput {
+}
